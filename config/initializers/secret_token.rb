19cc9bb9e0653f445675972c774e9453ce5e2c0e
MakerSquare Student <student@lab-1.local>
2013-07-08 15:39:41 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Readit::Application.config.secret_token = '2ea5c410d1b082eac7d10e34210d349ba675755be323cf7b5ef8498f29cec04efc7cd2ce223d51fe454e714949458353b4448dd2b1a89b6353463a9214f29117'
