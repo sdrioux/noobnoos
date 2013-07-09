cb881decb72609dbaec1f6ef2289f0002f368c90
MakerSquare Student <students@themakersquare.com>
2013-07-09 14:23:26 -0500

    add links model and associations to user table

---
+++ app/models/link.rb
@@ -0,0 +1,5 @@
+class Link < ActiveRecord::Base
+  attr_accessible :title, :url
+
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130708204733) do
+ActiveRecord::Schema.define(:version => 20130709191457) do
+
+  create_table "links", :force => true do |t|
+    t.string   "url"
+    t.string   "title"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "username"
