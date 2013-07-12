97c2d2d3d8fd08539a700232aebc84b52d847e7c
Silas Rioux <silas.rioux@gmail.com>
2013-07-12 15:22:31 -0500

    Fix pagination, so that it no longer orders by date.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
     params[:page] ||=1
     params[:per_page] ||=5
 
-    @links = Link.order("created_at DESC").page(params[:page].to_i).per_page(params[:per_page].to_i)
+    @links = Link.page(params[:page].to_i).per_page(params[:per_page].to_i)
   end
 
 end
