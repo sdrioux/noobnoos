ed40c040f2f3c08f1736a4513bba7028d70a38b7
Silas Rioux <silasrioux@unknown00236c7d0df3.gateway.2wire.net>
2013-08-11 20:52:55 -0500

    fix view problems from changing from 12 rows to 24 rows

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -3,9 +3,15 @@ module LinksHelper
   def show_favorite_star(user, link)
     #default favorite_star classes
     favorite_classes = "icon-star icon-large"
+
+    unless current_user.present?
+      link_to raw("<i class='#{favorite_classes + " icon-muted"}'></i>"), new_user_session_path
+    else
     #if the link is NOT a favorite for the user, add class to make the star gray.
     favorite_classes += " icon-muted" if !Favorite.exists?(user_id: user.id, link_id: link.id)
-    #display the star with the correct classes.
+    #display the star with the correct classes. 
     link_to_function raw("<i class='#{favorite_classes}'></i>"), "$.ajax({url:'/links/#{link.id}/favorite', type: 'PUT'});"
+    end
   end
+
 end
